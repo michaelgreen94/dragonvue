ea2d9a70dc6249bc355d4bca58179eb98147bd7c
Michael Green <michael.green94@hotmail.com>
2018-08-29 11:09:38 -0600

    firsts init

---
+++ src/views/Game.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="game">
+    <h1>Hello From Game</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {};
+    }
+  }
+</script>
+
+<style>
+</style>
